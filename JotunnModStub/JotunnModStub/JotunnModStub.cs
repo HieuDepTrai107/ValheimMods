2822cd46cd4dce0663c266cc5ace44f4ce5299f4
YarikMix <43493788+YarikMix@users.noreply.github.com>
2021-08-23 21:33:36 +0300

    Add JotunnModStub

---
+++ JotunnModStub/JotunnModStub/JotunnModStub.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+
+namespace JotunnModStub
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    public class JotunnModStub : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.jotunn.jotunnmodstub";
+        public const string PluginName = "JotunnModStub";
+        public const string PluginVersion = "0.0.1";
+
+        private void Awake()
+        {
+            Jotunn.Logger.LogMessage("Hello World!");
+        }
+
+    }
+}
